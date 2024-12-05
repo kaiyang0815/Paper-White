2a499703e81287cbe01356f4144e4d1718a2671b
kaiyang0815 <wangkaiyang0815@gmail.com>
2024-12-05 08:23:07 +0800

    first commit

---
+++ Paper White/CanvasView.swift
@@ -0,0 +1,18 @@
+//
+//  CanvasView.swift
+//  Paper White
+//
+//  Created by kaiyang on 2024/12/5.
+//
+
+import SwiftUI
+
+struct CanvasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CanvasView()
+}
