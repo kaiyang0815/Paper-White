ab1eabcebaeacd4393733ff36752f6dac6d4f80b
kaiyang0815 <wangkaiyang0815@gmail.com>
2024-12-05 08:24:15 +0800

    Add: 新增CanvasView

--- Paper White/CanvasView.swift
+++ Paper White/CanvasView.swift
@@ -5,14 +5,69 @@
 //  Created by kaiyang on 2024/12/5.
 //
 
+import PencilKit
 import SwiftUI
 
-struct CanvasView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CanvasView: UIViewRepresentable {
+    @Binding var drawing: PKDrawing
+    @Binding var toolPickerShows: Bool
+
+    private let canvasView = PKCanvasView()
+    private let toolPicker = PKToolPicker()
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.delegate = context.coordinator
+        // Allow finger drawing
+        canvasView.drawingPolicy = .anyInput
+
+        // Make the tool picker visible or invisible depending on toolPickerShows
+        toolPicker.setVisible(toolPickerShows, forFirstResponder: canvasView)
+        // Make the canvas respond to tool changes
+        toolPicker.addObserver(canvasView)
+
+        // Make the canvas active -- first responder
+        if toolPickerShows {
+            canvasView.becomeFirstResponder()
+        }
+
+        return canvasView
     }
+
+    func updateUIView(_ canvasView: PKCanvasView, context: Context) {
+        // Called when SwiftUI updates the view, (makeUIView(context:) called when creating the view.)
+        // For example, called when toolPickerShows is toggled:
+        // so hide or show tool picker
+
+        toolPicker.setVisible(toolPickerShows, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        if toolPickerShows {
+            canvasView.becomeFirstResponder()
+        } else {
+            canvasView.resignFirstResponder()
+        }
+        if drawing != canvasView.drawing {
+            canvasView.drawing = drawing
+        }
+    }
+
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var drawing: Binding<PKDrawing>
+
+        init(drawing: Binding<PKDrawing>) {
+            self.drawing = drawing
+        }
+
+        func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+            drawing.wrappedValue = canvasView.drawing
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(drawing: $drawing)
+    }
+
 }
 
 #Preview {
-    CanvasView()
+    CanvasView(drawing: .constant(PKDrawing()),toolPickerShows: .constant(true))
 }
